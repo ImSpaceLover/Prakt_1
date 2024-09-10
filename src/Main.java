780956d2c0b4582d0db45277054bb2dcd0f8fed6
I_like_Space <dandyl.dmitry@mail.ru>
2024-09-10 12:49:13 +0300

    Задание 5 (аргументы ком. строки (не работает)!)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import ru.mirea.lab1.Prakt_1;
 
+import java.util.Arrays;
+
 import static ru.mirea.lab1.Prakt_1.prakt_1;
 
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
+            System.out.printf(args[i]);
         }
+        System.out.println(Arrays.toString(args));
     }
 }
