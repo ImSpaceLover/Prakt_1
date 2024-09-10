95835bb9038fd6a218c06178428af94779880b6d
I_like_Space <dandyl.dmitry@mail.ru>
2024-09-10 13:03:13 +0300

    Задание 6 (Гармонический ряд)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
-import ru.mirea.lab1.Prakt_1;
-
-import static ru.mirea.lab1.Prakt_1.prakt_1;
-
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
+        float part_sum = 0;
+        for (int i = 1; i < 11; i++) {
+            part_sum += 1/(float)i;
+            System.out.printf("%d-й элемент гармонического ряда: ", i);
+            for (int j = 1; j < i; j++) {
+                System.out.printf("1/%d + ", j);
+            }
+            System.out.printf("1/%d = %f\n", i, part_sum);
         }
     }
 }
